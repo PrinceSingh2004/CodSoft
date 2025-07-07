245a3cfbd06db7c4afb72ab7bb24f1406714a3b4
Prince Singh <ps1263859@gmail.com>
2025-07-07 21:39:10 +0530

    Delete abc_1.py

--- abc_1.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-win=Tk()
-win.config(bg="pink")
-win.title("Sakshi Singh")
-win.iconbitmap(r"C:\Users\DELL\Downloads\sakshi .ico")
-label=Label(win,text="Sakshi Singh",font=("Edwardian Script ITC",30,"bold"),bg="lightpink")
-label.pack(padx=100,pady=300)
-win.mainloop()
